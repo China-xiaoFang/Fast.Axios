5822177dd549635bbb9add697f20f2c72d42e91f
小方 <2875616188@qq.com>
2025-02-08 16:22:56 +0800

    `v1.0.3` 增加 `iife` 构建

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig([
 	{
 		// 入口文件
-		entry: ["src/index.ts", "src/vite.ts", "src/webpack.ts"],
+		entry: ["src/vite.ts", "src/webpack.ts"],
 		// 输出目录
 		outDir: "dist",
 		// 输出格式
@@ -24,22 +24,55 @@ export default defineConfig([
 		external: ["axios"],
 	},
 	{
-		entry: ["src/index.ts", "src/vite.ts", "src/webpack.ts"],
+		globalName: "FastAxios",
+		// 入口文件
+		entry: ["src/index.ts"],
+		// 输出目录
 		outDir: "dist",
-		format: ["cjs", "esm"],
+		// 输出格式
+		format: ["iife", "cjs", "esm"],
+		// 生成类型定义文件
+		dts: true,
+		// 启用代码拆分
+		splitting: true,
+		// 生成 source map
+		sourcemap: true,
+		// 清理输出目录
+		clean: false,
+		// 压缩输出文件
+		minify: false,
+		// 去除未使用的代码
+		treeshake: true,
+		// 构建排除的包
+		external: ["axios"],
+	},
+	{
+		globalName: "FastAxios",
+		entry: ["src/index.ts"],
+		outDir: "dist",
+		format: ["iife", "cjs", "esm"],
 		dts: false,
 		splitting: true,
 		sourcemap: true,
-		clean: true,
+		clean: false,
 		minify: true,
 		treeshake: true,
 		// 构建排除的包
 		external: ["axios"],
 		// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 		outExtension({ format }) {
-			return {
-				js: format === "cjs" ? ".min.cjs" : ".min.js",
-			};
+			let ext;
+			switch (format) {
+				case "cjs":
+					ext = ".min.cjs";
+					break;
+				case "iife":
+					ext = ".global.min.js";
+					break;
+				default:
+					ext = ".min.js";
+			}
+			return { js: ext };
 		},
 	},
 ]);
